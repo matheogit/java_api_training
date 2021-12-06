a4fe6bd00dc36165dcab4b3196fe1db480f19325
Louerat Matheo <louerat@et.esiea.fr>
2021-12-06 20:10:27 +0100

    Server HTTP

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,36 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class Launcher {
     public static void main(String[] args){
-
+        try {
+            int socket_adress = Integer.parseInt(args[0]);
+            HttpServer server = HttpServer.create(new InetSocketAddress(socket_adress), 0);
+            server.createContext("/ping", new MyHandler());
+            server.setExecutor(new ThreadPoolExecutor(1, 1, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>()));
+            server.start();
+        } catch(java.io.IOException e) {
+            System.out.println("error server");
+        }
+    }
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String response = "OK";
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
